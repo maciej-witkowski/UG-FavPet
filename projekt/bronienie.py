efc6fb5a2d71fae9ed8d08ec0172a75083314eea
maciej-witkowski <=maciej.jan.witkowski@gmail.com>
2019-12-28 20:42:09 +0100

    dwie gry (karmienie i bronienie) w wersji alpha

---
+++ projekt/bronienie.py
@@ -0,0 +1,408 @@
+import pygame
+import random
+from pygame.locals import *
+pygame.init()
+
+display_width = 1000
+display_height = 600
+
+monster_width = 150
+monster_height = 150
+
+enemy_width = 75
+enemy_height = 75
+
+white = (255, 255, 255)
+blue = (0, 119, 204)
+blue_light = (145, 226, 245)
+blue_dark = (0, 0, 128)
+
+font_small = pygame.font.Font('fonts/orangejuice.ttf', 30)
+font_medium = pygame.font.Font('fonts/orangejuice.ttf', 50)
+font_big = pygame.font.Font('fonts/orangejuice.ttf', 120)
+
+main_music = pygame.mixer.Sound('sounds/music_defend.wav')
+kill_sound = pygame.mixer.Sound('sounds/kill_sound.wav')
+hurt_sound = pygame.mixer.Sound('sounds/oof.wav')
+
+wybor_postaci = pygame.image.load('screens/CHOOSEv1.png')
+
+hp_bar = pygame.image.load('bars/hpbar_horizontal.png')
+hp_cover = pygame.image.load('bars/white_pallet.png')
+
+enemy1 = pygame.image.load('enemy_img/enemy1icon.png')
+enemy2 = pygame.image.load('enemy_img/enemy2icon.png')
+enemy3 = pygame.image.load('enemy_img/enemy3icon.png')
+enemy4 = pygame.image.load('enemy_img/enemy5icon.png')
+
+enemies = [enemy1, enemy2, enemy3, enemy4]
+
+monster1 = pygame.image.load('monsters_icons_img/MONSTER1_game.png')
+monster2 = pygame.image.load('monsters_icons_img/MONSTER2_game.png')
+monster3 = pygame.image.load('monsters_icons_img/MONSTER3_game.png')
+monster4 = pygame.image.load('monsters_icons_img/MONSTER4_game.png')
+monster5 = pygame.image.load('monsters_icons_img/MONSTER5_game.png')
+monster6 = pygame.image.load('monsters_icons_img/MONSTER6_game.png')
+
+choose_screen = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption("FavPet: OBRONA")
+
+clock = pygame.time.Clock()
+
+
+def which_monster():
+    while True:
+        choose_screen.blit(wybor_postaci, (0, 0))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                quit()
+            if event.type == KEYDOWN:
+                if event.key == K_1:
+                    return monster1
+                elif event.key == K_2:
+                    return monster2
+                elif event.key == K_3:
+                    return monster3
+                elif event.key == K_4:
+                    return monster4
+                elif event.key == K_5:
+                    return monster5
+                elif event.key == K_6:
+                    return monster6
+
+
+def monster(x, y, img):
+    screen.blit(img, (x, y))
+
+
+def spawn_enemy(place, x, y):
+    if place == 0:
+        screen.blit(enemies[2], (x, y))
+    elif place == 1:
+        screen.blit(enemies[1], (x, y))
+    elif place == 2:
+        screen.blit(enemies[0], (x, y))
+    elif place == 3:
+        screen.blit(enemies[3], (x, y))
+
+
+def reset_enemy_pos(pos_enemies, which):
+    if which == 0:
+        pos_enemies[0][0] = 0
+        pos_enemies[0][1] = 0
+    elif which == 1:
+        pos_enemies[1][0] = 1325
+        pos_enemies[1][1] = 0
+    elif which == 2:
+        pos_enemies[2][0] = 1325
+        pos_enemies[2][1] = 675
+    elif which == 3:
+        pos_enemies[3][0] = 0
+        pos_enemies[3][1] = 675
+
+
+def kill_count(count):
+    text = font_medium.render("Zestrzelone: "+str(count), True, blue)
+    screen.blit(text, (display_width // 2, 700))
+
+
+def hp(count):
+    screen.blit(hp_bar, (435, 10))
+    if count == 1:
+        screen.blit(hp_cover, (919, 0))
+    elif count == 2:
+        screen.blit(hp_cover, (865, 0))
+    elif count == 3:
+        screen.blit(hp_cover, (811, 0))
+    elif count == 4:
+        screen.blit(hp_cover, (757, 0))
+    elif count == 5:
+        screen.blit(hp_cover, (703, 0))
+    elif count == 6:
+        screen.blit(hp_cover, (649, 0))
+    elif count == 7:
+        screen.blit(hp_cover, (595, 0))
+    elif count == 8:
+        screen.blit(hp_cover, (541, 0))
+    elif count == 9:
+        screen.blit(hp_cover, (487, 0))
+
+
+def levels(count):
+    text = font_medium.render("Poziom: "+str(count), True, blue)
+    screen.blit(text, (display_width // 4 + 50, 700))
+
+
+def set_enemy_speed(level):
+    if level == 1:
+        x = random.randrange(2, 5)
+        if x % 2 == 1:
+            x += 1
+        y = x // 2
+        return x, y
+    elif level == 2:
+        x = random.randrange(6, 9)
+        if x % 2 == 1:
+            x += 1
+        y = x // 2
+        return x, y
+    elif level == 3:
+        x = random.randrange(9, 12)
+        if x % 2 == 1:
+            x += 1
+        y = x // 2
+        return x, y
+    elif level == 4:
+        x = random.randrange(12, 15)
+        if x % 2 == 1:
+            x += 1
+        y = x // 2
+        return x, y
+    elif level == 5:
+        x = random.randrange(15, 19)
+        if x % 2 == 1:
+            x += 1
+        y = x // 2
+        return x, y
+
+
+def message_display(text, size, place_width, place_height, tone):
+    text_surface = size.render(text, True, tone)
+    text_rect = text_surface.get_rect()
+    text_rect.center = ((place_width//2), (place_height//2))
+    screen.blit(text_surface, text_rect)
+    pygame.display.update()
+
+
+def button(text, x, y, w, h, ia_c, a_c, action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(screen, a_c, (x, y, w, h))
+        if click[0] == 1 and action is not None:
+            action()
+    else:
+        pygame.draw.rect(screen, ia_c, (x, y, w, h))
+    message_display(text, font_small, (x+(w//2)) * 2, (y+(h//2)) * 2, white)
+
+
+def music(channel, action, file):
+    if action == "play":
+        pygame.mixer.Channel(channel).play(pygame.mixer.Sound(file))
+    elif action == "stop":
+        pygame.mixer.Channel(channel).stop()
+
+
+def unpaused():
+    global pause
+    music(0, "play", main_music)
+    pause = False
+
+
+def paused():
+    while pause:
+        music(0, "stop", main_music)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quitgame()
+        message_display("Pauza...", font_big, display_width, display_height - 200, blue)
+        button("Kontynuuj", 350, 550, 300, 50, blue, blue_light, unpaused)
+        button("Zakończ.", 750, 550, 300, 50, blue, blue_light, quitgame)
+
+
+def game_over(kill, level):
+    music(0, "stop", main_music)
+    screen.fill(white)
+    message_display("PRZEGRAŁEŚ :(", font_big, display_width, display_height, blue)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quitgame()
+        button("Zagraj ponownie!", 350, 550, 300, 50, blue, blue_light, main)
+        button("Zakończ.", 750, 550, 300, 50, blue, blue_light, quitgame)
+        message_display("Twoje statystyki ", font_medium, display_width, int(display_height // 4), blue_dark)
+        message_display("Zestrzelone: "+str(kill), font_medium, int(display_width*1/3), display_height//2, blue_dark)
+        text = font_medium.render("Poziom: " + str(level), True, blue_dark)
+        screen.blit(text, (1100, 165))
+
+
+def quitgame():
+    pygame.quit()
+    quit()
+
+
+def main():
+    """ USTAWIENIE DO POPRAWNEGO DZIAŁA FUNKCJI PAUZY """
+    global pause
+
+    ''' ROZPOCZĘCIE GRANIA MUZYKI '''
+    music(0, "play", main_music)
+
+    ''' DEFINIOWANIE POZYCJI MONSTERA '''
+    x_monster = 625
+    y_monster = 300
+
+    ''' DEFINIOWANIE POZYCJI WROGÓW '''
+    pos_enemies = [[0, 0], [1325, 0], [1325, 675], [0, 675]]
+
+    ''' DEFINIOWANIE PRĘDKOŚCI WROGÓW '''
+    x_change_e = 2
+    y_change_e = 1
+
+    ''' DEFINIOWANIE STATYSTYK W GRZE '''
+    kill = 0
+    hit = 0
+    level = 1
+
+    ''' DEFINIOWANIE POCZĄTKOWEJ POZYCJI WROGA'''
+    enemy = random.randrange(0, 4)
+
+    while True:
+        ''' STAŁE POZYSKIWANIE DANYCH NA TEMAT DZIAŁANIA MYSZKI '''
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        ''' EVENTY W PYGAME '''
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                quit()
+            if event.type == KEYDOWN:
+                if event.key == K_p:
+                    pause = True
+                    paused()
+
+        ''' PRZYGOTOWANIE EKRANU '''
+        screen.fill(white)
+        monster(x_monster, y_monster, monsterImg)
+        kill_count(kill)
+        hp(hit)
+        levels(level)
+        spawn_enemy(enemy, pos_enemies[enemy][0], pos_enemies[enemy][1])
+
+        ''' ZMIANA POZYCJI WROGÓW '''
+        if enemy == 0:
+            pos_enemies[0][0] += x_change_e
+            pos_enemies[0][1] += y_change_e
+        elif enemy == 1:
+            pos_enemies[1][0] -= x_change_e
+            pos_enemies[1][1] += y_change_e
+        elif enemy == 2:
+            pos_enemies[2][0] -= x_change_e
+            pos_enemies[2][1] -= y_change_e
+        elif enemy == 3:
+            pos_enemies[3][0] += x_change_e
+            pos_enemies[3][1] -= y_change_e
+
+        ''' ZDERZENIE WROGA Z POTWORKIEM I WSZYSTKIE AKCJE Z TYM ZWIĄZANE, CZYLI:
+            - RESETOWANIE POZYCI PO ZABÓJSTWIE,
+            - LOSOWANIE POZYCJI KOLEJNEGO WROGA,
+            - ZMIEJSZANIE ILOŚCI ŻYĆ,
+            - LOSOWANIE PRĘDKOŚCI PRZECIWNIKA,
+            - ZARZĄDZANIE DŹWIEKAMI '''
+        if (pos_enemies[0][0] + enemy_width > x_monster) and (pos_enemies[0][1] + enemy_height > y_monster):
+            music(2, "play", hurt_sound)
+            reset_enemy_pos(pos_enemies, 0)
+            enemy = random.randrange(0, 4)
+            hit += 1
+            x_change_e, y_change_e = set_enemy_speed(1)
+
+        elif (pos_enemies[1][0] < x_monster + monster_width) and (pos_enemies[1][1] + enemy_height > y_monster):
+            music(2, "play", hurt_sound)
+            reset_enemy_pos(pos_enemies, 1)
+            enemy = random.randrange(0, 4)
+            hit += 1
+            x_change_e, y_change_e = set_enemy_speed(1)
+
+        elif (pos_enemies[2][0] < x_monster + monster_width) and (pos_enemies[2][1] < y_monster + monster_height):
+            music(2, "play", hurt_sound)
+            reset_enemy_pos(pos_enemies, 2)
+            enemy = random.randrange(0, 4)
+            hit += 1
+            x_change_e, y_change_e = set_enemy_speed(1)
+
+        elif (pos_enemies[3][0] + enemy_width > x_monster) and (pos_enemies[3][1] < y_monster + monster_height):
+            music(2, "play", hurt_sound)
+            reset_enemy_pos(pos_enemies, 3)
+            enemy = random.randrange(0, 4)
+            hit += 1
+            x_change_e, y_change_e = set_enemy_speed(1)
+
+        ''' ZABIJANIE POTWORÓW I WSZYSTKIE AKCJI Z TYM ZWIĄZANE, CZYLI:
+            - RESETOWANIE POZYCI PO ZABÓJSTWIE,
+            - LOSOWANIE POZYCJI KOLEJNEGO WROGA,
+            - ZWIĘKSZANIE LICZNIKA ZABÓJSTW,
+            - LOSOWANIE PRĘDKOŚCI PRZECIWNIKA,
+            - ZARZĄDZANIE DŹWIEKAMI '''
+        if pos_enemies[0][0] + enemy_width > mouse[0] > pos_enemies[0][0] and \
+                pos_enemies[0][1] + enemy_height > mouse[1] > pos_enemies[0][1]:
+            if click[0] == 1:
+                reset_enemy_pos(pos_enemies, 0)
+                enemy = random.randrange(0, 4)
+                kill += 1
+                x_change_e, y_change_e = set_enemy_speed(1)
+                if kill % 2 == 0:
+                    music(1, "play", kill_sound)
+
+        elif pos_enemies[1][0] + enemy_width > mouse[0] > pos_enemies[1][0] and\
+                pos_enemies[1][1] + enemy_height > mouse[1] > pos_enemies[1][1]:
+            if click[0] == 1:
+                reset_enemy_pos(pos_enemies, 1)
+                enemy = random.randrange(0, 4)
+                kill += 1
+                x_change_e, y_change_e = set_enemy_speed(1)
+                if kill % 2 == 0:
+                    music(1, "play", kill_sound)
+
+        elif pos_enemies[2][0] + enemy_width > mouse[0] > pos_enemies[2][0] and\
+                pos_enemies[2][1] + enemy_height > mouse[1] > pos_enemies[2][1]:
+            if click[0] == 1:
+                reset_enemy_pos(pos_enemies, 2)
+                enemy = random.randrange(0, 4)
+                kill += 1
+                x_change_e, y_change_e = set_enemy_speed(1)
+                if kill % 2 == 0:
+                    music(1, "play", kill_sound)
+
+        elif pos_enemies[3][0] + enemy_width > mouse[0] > pos_enemies[3][0] and\
+                pos_enemies[3][1] + enemy_height > mouse[1] > pos_enemies[3][1]:
+            if click[0] == 1:
+                reset_enemy_pos(pos_enemies, 3)
+                enemy = random.randrange(0, 4)
+                kill += 1
+                x_change_e, y_change_e = set_enemy_speed(1)
+                if kill % 2 == 0:
+                    music(1, "play", kill_sound)
+
+        ''' ZWIĘKSZANIE TRUDNOŚCI GRY '''
+        if kill > 9:
+            x_change_e, y_change_e = set_enemy_speed(2)
+            level = 2
+        if kill > 19:
+            x_change_e, y_change_e = set_enemy_speed(3)
+            level = 3
+        if kill > 29:
+            x_change_e, y_change_e = set_enemy_speed(4)
+            level = 4
+        # POZIOM DLA PROSÓW ;)
+        if kill > 99:
+            x_change_e, y_change_e = set_enemy_speed(5)
+            level = 9000
+
+        ''' GDY WYCZERPIEMY LIMIT ŻYĆ GRA SIĘ KOŃCZY '''
+        if hit > 9:
+            game_over(kill, level)
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+monsterImg = which_monster()
+display_width = 1400
+display_height = 750
+screen = pygame.display.set_mode((display_width, display_height))
+pause = False
+main()
+quitgame()
---
+++ projekt/karmienie.py
@@ -0,0 +1,354 @@
+import pygame
+import random
+from pygame.locals import *
+
+pygame.init()
+
+display_width = 1000
+display_height = 600
+
+monster_width = 150
+monster_height = 150
+object_width = 75
+object_height = 75
+
+white = (255, 255, 255)
+blue = (0, 119, 204)
+blue_light = (145, 226, 245)
+blue_dark = (0, 0, 128)
+
+font_small = pygame.font.Font('fonts/orangejuice.ttf', 30)
+font_medium = pygame.font.Font('fonts/orangejuice.ttf', 50)
+font_big = pygame.font.Font('fonts/orangejuice.ttf', 120)
+
+bite_sound = pygame.mixer.Sound('sounds/bite.wav')
+jazz_music = pygame.mixer.Sound('sounds/jazz_musicv2.wav')
+
+wybor_postaci = pygame.image.load('screens/CHOOSEv1.png')
+
+HPbar_vertical = pygame.image.load('bars/hpbar_vertical.png')
+
+HPcover = pygame.image.load('bars/white_pallet.png')
+
+HPcover_vert = pygame.image.load('bars/white_pallet_vert.png')
+
+Bar = pygame.image.load('bars/chicken_ia_bar.png')
+Bar2 = pygame.image.load('bars/chicken_ac_bar.png')
+
+food1 = pygame.image.load('food_img/tomato.png')
+food2 = pygame.image.load('food_img/cucumber.png')
+food3 = pygame.image.load('food_img/cabbage.png')
+food4 = pygame.image.load('food_img/apple.png')
+food5 = pygame.image.load('food_img/orange.png')
+food6 = pygame.image.load('food_img/watermelon.png')
+food7 = pygame.image.load('food_img/special1.png')
+food8 = pygame.image.load('food_img/poo.png')
+
+food = (food1, food2, food3, food4, food5, food6, food7, food8)
+
+monster1 = pygame.image.load('monsters_icons_img/MONSTER1_game.png')
+monster2 = pygame.image.load('monsters_icons_img/MONSTER2_game.png')
+monster3 = pygame.image.load('monsters_icons_img/MONSTER3_game.png')
+monster4 = pygame.image.load('monsters_icons_img/MONSTER4_game.png')
+monster5 = pygame.image.load('monsters_icons_img/MONSTER5_game.png')
+monster6 = pygame.image.load('monsters_icons_img/MONSTER6_game.png')
+
+screen_choose = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption("FavPet: KARMIENIE")
+clock = pygame.time.Clock()
+
+
+def which_monster():
+    while True:
+        screen_choose.blit(wybor_postaci, (0, 0))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                quit()
+            if event.type == KEYDOWN:
+                if event.key == K_1:
+                    return monster1
+                elif event.key == K_2:
+                    return monster2
+                elif event.key == K_3:
+                    return monster3
+                elif event.key == K_4:
+                    return monster4
+                elif event.key == K_5:
+                    return monster5
+                elif event.key == K_6:
+                    return monster6
+
+
+def monster(x, y, img):
+    screen.blit(img, (x, y))
+
+
+def which_object(lista):
+    count = random.randrange(0, 8)
+    if count == 0:
+        return lista[0]
+    elif count == 1:
+        return lista[1]
+    elif count == 2:
+        return lista[2]
+    elif count == 3:
+        return lista[3]
+    elif count == 4:
+        return lista[4]
+    elif count == 5:
+        return lista[5]
+    elif count == 6:
+        return lista[6]
+    elif count == 7:
+        return lista[7]
+
+
+def objects(x, y, img):
+    screen.blit(img, (x, y))
+
+
+def message_display(text, size, place_width, place_height, tone):
+    text_surface = size.render(text, True, tone)
+    text_rect = text_surface.get_rect()
+    text_rect.center = ((place_width // 2), (place_height // 2))
+    screen.blit(text_surface, text_rect)
+    pygame.display.update()
+
+
+def button(text, x, y, w, h, ia_c, a_c, action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    if x + w > mouse[0] > x and y + h > mouse[1] > y:
+        pygame.draw.rect(screen, a_c, (x, y, w, h))
+        if click[0] == 1 and action is not None:
+            action()
+    else:
+        pygame.draw.rect(screen, ia_c, (x, y, w, h))
+    message_display(text, font_small, (x + (w // 2)) * 2, (y + (h // 2)) * 2, white)
+
+
+def game_over(count, level):
+    music(0, "stop", jazz_music)
+    screen.fill(white)
+    message_display("PRZEGRAŁEŚ :(", font_big, display_width, display_height, blue)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quitgame()
+        button("Zagraj ponownie!", 350, 550, 300, 50, blue, blue_light, main)
+        button("Zakończ.", 750, 550, 300, 50, blue, blue_light, quitgame)
+        message_display("Twoje statystyki: ", font_medium, display_width, int(display_height // 4), blue_dark)
+        message_display("Zjedzone: " + str(count), font_medium, int(display_width * 1 / 3), display_height // 2,
+                        blue_dark)
+        text = font_medium.render("Poziom: " + str(level), True, blue_dark)
+        screen.blit(text, (1050, 165))
+        pygame.display.update()
+
+
+def unpaused():
+    music(0, "play", jazz_music)
+    global pause
+    pause = False
+
+
+def paused():
+    while pause:
+        music(0, "stop", jazz_music)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quitgame()
+        message_display("Pauza...", font_big, display_width, display_height, blue)
+        button("Kontynuuj", 350, 550, 300, 50, blue, blue_light, unpaused)
+        button("Zakończ.", 750, 550, 300, 50, blue, blue_light, quitgame)
+        pygame.display.update()
+
+
+def play_or_end():
+    while pause:
+        music(0, "stop", jazz_music)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quitgame()
+        message_display("Nakarmiłeś FavPeta!", font_big, display_width, display_height // 2, blue)
+        message_display("Czy chcesz dalej kontynuować grę?", font_medium, display_width, display_height, blue_dark)
+        button("Kontynuuj", 350, 550, 300, 50, blue, blue_light, unpaused)
+        button("Zakończ.", 750, 550, 300, 50, blue, blue_light, quitgame)
+        pygame.display.update()
+
+
+def quitgame():
+    pygame.quit()
+    quit()
+
+
+def ate(count):
+    text = font_medium.render("Zjedzone: " + str(count), True, blue)
+    screen.blit(text, (display_width - 260, 5))
+
+
+def levels(level):
+    text = font_medium.render("Poziom: " + str(level), True, blue)
+    screen.blit(text, (5, 5))
+
+
+def hp(count):
+    screen.blit(HPbar_vertical, (5, display_height - 630))
+    if count == 1:
+        screen.blit(HPcover_vert, (5, 602))
+    elif count == 2:
+        screen.blit(HPcover_vert, (5, 548))
+    elif count == 3:
+        screen.blit(HPcover_vert, (5, 494))
+    elif count == 4:
+        screen.blit(HPcover_vert, (5, 440))
+    elif count == 5:
+        screen.blit(HPcover_vert, (5, 386))
+    elif count == 6:
+        screen.blit(HPcover_vert, (5, 332))
+    elif count == 7:
+        screen.blit(HPcover_vert, (5, 278))
+    elif count == 8:
+        screen.blit(HPcover_vert, (5, 224))
+    elif count == 9:
+        screen.blit(HPcover_vert, (5, 170))
+
+
+def food_bar(count):
+    screen.blit(Bar, (display_width - 51, display_height - 655))
+    if 4 < count < 10:
+        screen.blit(Bar2, (display_width - 51, 645))
+        screen.blit(HPcover_vert, (display_width - 51, 700))
+    elif 9 < count < 15:
+        screen.blit(Bar2, (display_width - 51, 584))
+        screen.blit(HPcover_vert, (display_width - 51, 700))
+    elif 14 < count < 20:
+        screen.blit(Bar2, (display_width - 51, 523))
+        screen.blit(HPcover_vert, (display_width - 51, 700))
+    elif 19 < count < 25:
+        screen.blit(Bar2, (display_width - 51, 462))
+        screen.blit(HPcover_vert, (display_width - 51, 700))
+    elif 24 < count < 30:
+        screen.blit(Bar2, (display_width - 51, 401))
+        screen.blit(HPcover_vert, (display_width - 51, 700))
+    elif 29 < count < 35:
+        screen.blit(Bar2, (display_width - 51, 340))
+        screen.blit(HPcover_vert, (display_width - 51, 700))
+    elif 34 < count < 40:
+        screen.blit(Bar2, (display_width - 51, 279))
+        screen.blit(HPcover_vert, (display_width - 51, 700))
+    elif 39 < count < 45:
+        screen.blit(Bar2, (display_width - 51, 218))
+        screen.blit(HPcover_vert, (display_width - 51, 700))
+    elif 44 < count < 50:
+        screen.blit(Bar2, (display_width - 51, 157))
+        screen.blit(HPcover_vert, (display_width - 51, 700))
+    elif count > 49:
+        screen.blit(Bar2, (display_width - 51, 96))
+        screen.blit(HPcover_vert, (display_width - 51, 700))
+
+
+def music(channel, action, file):
+    if action == "play":
+        pygame.mixer.Channel(channel).play(pygame.mixer.Sound(file))
+    elif action == "stop":
+        pygame.mixer.Channel(channel).stop()
+
+
+def main():
+    global pause
+
+    music(0, "play", jazz_music)
+
+    x_monster = int(display_width * 0.43)
+    y_monster = int(display_height * 0.80)
+
+    x_change = 0
+
+    object_startx = random.randrange(0 + 55, display_width - object_width - 51)
+    object_starty = -display_height
+    object_speed = 4
+    objectimg = which_object(food)
+
+    catched = 0
+    missed = 0
+    level = 1
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                quit()
+            if event.type == KEYDOWN:
+                if event.key == K_a:
+                    x_change = -10
+                elif event.key == K_d:
+                    x_change = 10
+                elif event.key == K_p:
+                    pause = True
+                    paused()
+            if event.type == KEYUP:
+                if event.key == K_a or event.key == K_d:
+                    x_change = 0
+
+        screen.fill(white)
+
+        monster(x_monster, y_monster, monsterImg)
+        objects(object_startx, object_starty, objectimg)
+
+        levels(level)
+        hp(missed)
+        food_bar(catched)
+        ate(catched)
+
+        object_starty += object_speed
+        x_monster += x_change
+
+        if x_monster > display_width - monster_width - 51 or x_monster < 0 + 55:
+            x_monster -= x_change
+
+        if object_starty > display_height - object_height:
+            if objectimg != food[6] and objectimg != food[7]:
+                missed += 1
+            objectimg = which_object(food)
+            object_starty = 0
+            object_startx = random.randrange(0 + 55, display_width - object_width - 51)
+
+        if object_starty + object_height > y_monster:
+            if x_monster + monster_width > object_startx and x_monster < object_startx + object_width:
+                music(1, "play", bite_sound)
+                if objectimg == food[6]:
+                    missed -= 1
+                elif objectimg == food[7]:
+                    missed += 1
+                else:
+                    catched += 1
+                if catched % 5 == 0:
+                    if objectimg != food[6] and objectimg != food[7]:
+                        level += 1
+                        object_speed += 1
+                objectimg = which_object(food)
+                object_starty = 0
+                object_startx = random.randrange(0 + 55, display_width - object_width - 51)
+                if catched == 50:
+                    pause = True
+                    play_or_end()
+
+        if missed < 0:
+            missed = 0
+
+        if missed > 9:
+            game_over(catched, level)
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+monsterImg = which_monster()
+display_width = 1400
+display_height = 750
+screen = pygame.display.set_mode((display_width, display_height))
+pause = False
+main()
+quitgame()
